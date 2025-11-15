f783a23cf0a8a8f5579b0873b711ee9eb956f296
karen-malka <ks959@cornell.edu>
2025-11-15 17:28:01 -0500

    learn more pages added

--- scripts/nav.js
+++ scripts/nav.js
@@ -24,6 +24,20 @@ $("#applefest").click(function () {
   window.open("https://karen-malka.github.io/apple-fest/", "_blank");
 });
 
+$("#asset-management").click(function () {
+  window.open(
+    "https://www.figma.com/proto/67sdgVBRLS03E303Unq9FP/Daikin-Building-Walkthrough-Assets?node-id=1-2&starting-point-node-id=1%3A2&t=gp7XM7PYbGb45dTm-1",
+    "_blank"
+  );
+});
+
+$("#greengo").click(function () {
+  window.open(
+    "https://www.figma.com/proto/snuaP7dCBXG1gZxHYpgJuJ/GreEngine-Prototype?node-id=497-139&p=f&t=afClwpPXxSaTboUU-8&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=497%3A139&show-proto-sidebar=1&hide-ui=1",
+    "_blank"
+  );
+});
+
 $("#home-button").click(function () {
   if (page == "index.html") {
     $("html, body").animate(
