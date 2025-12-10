27a8ec655f199223ad4287d49a2c340142c38329
karen-malka <ks959@cornell.edu>
2025-12-10 12:14:22 -0500

    changes to nav made

--- scripts/nav.js
+++ scripts/nav.js
@@ -12,36 +12,6 @@ $(document).ready(function () {
   }
 });
 
-$("#waterford").click(function () {
-  window.open("https://karen-malka.github.io/research-at-waterford/", "_blank");
-});
-
-$("#artalcove").click(function () {
-  window.open("http://4300showcase.infosci.cornell.edu:5229/", "_blank");
-});
-
-$("#applefest").click(function () {
-  window.open("https://karen-malka.github.io/apple-fest/", "_blank");
-});
-
-$("#bearbargain").click(function () {
-  window.open("http://bear-bargain.myartsonline.com/", "_blank");
-});
-
-$("#asset-management").click(function () {
-  window.open(
-    "https://www.figma.com/proto/67sdgVBRLS03E303Unq9FP/Daikin-Building-Walkthrough-Assets?node-id=1-2&starting-point-node-id=1%3A2&t=gp7XM7PYbGb45dTm-1",
-    "_blank"
-  );
-});
-
-$("#greengo").click(function () {
-  window.open(
-    "https://www.figma.com/proto/snuaP7dCBXG1gZxHYpgJuJ/GreEngine-Prototype?node-id=497-139&p=f&t=afClwpPXxSaTboUU-8&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=497%3A139&show-proto-sidebar=1&hide-ui=1",
-    "_blank"
-  );
-});
-
 $("#home-button").click(function () {
   if (page == "index.html") {
     $("html, body").animate(
@@ -56,58 +26,76 @@ $("#home-button").click(function () {
 });
 
 $("#projects-button").click(function () {
-  if (page == "index.html") {
-    $("html, body").animate(
-      {
-        scrollTop: $("#projects").offset().top - 100,
-      },
-      1000
-    );
-  } else {
-    window.location.href = "index.html#projects";
-  }
+  window.location.href = "projects.html";
 });
 
 $("#websites-button").click(function () {
-  $("html, body").animate(
-    { scrollTop: $("#websites").offset().top - 100 },
-    1000
-  );
-});
-
-$("#prototypes-button").click(function () {
-  $("html, body").animate(
-    { scrollTop: $("#prototypes").offset().top - 100 },
-    1000
-  );
-});
-
-$("#photographs-button").click(function () {
-  $("html, body").animate(
-    { scrollTop: $("#photographs").offset().top - 100 },
-    1000
-  );
-});
-
-$("#art-button").click(function () {
-  $("html, body").animate({ scrollTop: $("#art").offset().top - 100 }, 1000);
-});
-
-$("#flyers-button").click(function () {
-  $("html, body").animate({ scrollTop: $("#flyers").offset().top - 100 }, 1000);
-});
-
-$("#designs-button").click(function () {
-  if (page == "index.html") {
+  if (page == "projects.html") {
     $("html, body").animate(
       {
-        scrollTop: $("#designs").offset().top - 100,
+        scrollTop: $("#websites").offset().top - 100,
       },
       1000
     );
   } else {
-    window.location.href = "index.html#designs";
+    window.location.href = "projects.html#websites";
   }
+});
+
+$("#prototypes-button").click(function () {
+  if (page == "projects.html") {
+    $("html, body").animate(
+      {
+        scrollTop: $("#prototypes").offset().top - 100,
+      },
+      1000
+    );
+  } else {
+    window.location.href = "projects.html#prototypes";
+  }
+});
+
+$("#photographs-button").click(function () {
+  if (page == "designs.html") {
+    $("html, body").animate(
+      {
+        scrollTop: $("#photographs").offset().top - 100,
+      },
+      1000
+    );
+  } else {
+    window.location.href = "designs.html#photographs";
+  }
+});
+
+$("#art-button").click(function () {
+  if (page == "designs.html") {
+    $("html, body").animate(
+      {
+        scrollTop: $("#art").offset().top - 100,
+      },
+      1000
+    );
+  } else {
+    window.location.href = "designs.html#art";
+  }
+});
+
+$("#flyers-button").click(function () {
+  if (page == "designs.html") {
+    $("html, body").animate(
+      {
+        scrollTop: $("#flyers").offset().top - 100,
+      },
+      1000
+    );
+  } else {
+    window.location.href = "designs.html#flyers";
+  }
+});
+
+$("#designs-button").click(function () {
+  window.location.href = "designs.html";
 });
 
 $("#about-button").click(function () {
